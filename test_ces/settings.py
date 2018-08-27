58bc15e5dc5bd139a4946a3ffe03a299639c1f89
Faizan Shaikh <fshaikh@ces.local>
2018-08-27 17:52:29 +0530

    Updated setting

--- test_ces/settings.py
+++ test_ces/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rank'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
